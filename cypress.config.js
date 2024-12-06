9b4f81d7e349d2a0f1ff2307f0d75d0e3f06959b
Alon Gross <alongross75@gmail.com>
2024-12-06 18:33:36 +0200

    Initial Cypress-Metamask setup with tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,25 @@
 const { defineConfig } = require('cypress');
-const metamask = require('cypress-metamask/plugins/index.js');
+const path = require('path');
 
 module.exports = defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
-            metamask(on, config); // Load the MetaMask plugin
+            on('before:browser:launch', (browser = {}, launchOptions) => {
+                if (browser.name === 'chrome' || browser.name === 'chromium') {
+                    // Add MetaMask extension path
+                    launchOptions.extensions.push(path.resolve('path_to_metamask_extension'));
+                }
+
+                // Remove unsupported properties
+                delete launchOptions.width;
+                delete launchOptions.height;
+                delete launchOptions.resizable;
+
+                return launchOptions;
+            });
             return config;
         },
         baseUrl: 'https://treasury.coti.io',
-        video: true,
         supportFile: 'cypress/support',
     },
 });
